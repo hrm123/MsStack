08e92c34f21047eb3de57b65748210e809a16ab2
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2018-06-17 17:52:42 -0400

    working from postman and anypoint

--- WcfSamples/WcfSamples/Service1.svc.cs
+++ WcfSamples/WcfSamples/Service1.svc.cs
@@ -15,6 +15,42 @@ namespace WcfSamples
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
+
+            /*
+             * example call from postman
+             * 
+             * 
+             Body - <?xml version="1.0" encoding="UTF-8" ?>
+            <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+            <s:Body>
+            <GetData xmlns="http://tempuri.org/">
+            <value>0</value>
+            </GetData>
+            </s:Body>
+            </s:Envelope>
+            Headers - 
+              [{"key":"Content-Type","value":"text/xml","description":""}]
+            [{"key":"SOAPAction","value":"http://tempuri.org/IService1/GetData","description":""}] 
+             */
+
+            /*
+             Example config in Mulesoft
+             <http:listener-config doc:name="HTTP Listener Configuration" host="localhost" name="HTTP_Listener_Configuration" port="8081" basePath="testwcf1"/>
+            <http:request-config doc:name="HTTP Request Configuration" host="localhost" name="HTTP_Request_Configuration" port="49168"/>
+            <flow name="soap-api-proxyFlow">
+                <http:listener config-ref="HTTP_Listener_Configuration" path="/" doc:name="HTTP"/>
+                <http:request config-ref="HTTP_Request_Configuration" doc:name="Send Requests to API" method="POST" path="/Service1.svc">
+                    <http:request-builder>
+                        <http:header headerName="Content-Type" value="text/xml"/>
+                        <http:header headerName="SOAPAction" value="http://tempuri.org/IService1/GetData"/>
+                    </http:request-builder>
+                </http:request>
+            </flow>
+
+            then post req from postman
+            http://localhost:8081/testwcf1
+            with sam eboady as above postman req.
+             */
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
