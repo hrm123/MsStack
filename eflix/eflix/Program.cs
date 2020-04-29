4d4a429dd2108ec7f4f923c404d61d02b95b872b
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2020-04-29 08:55:09 -0400

    eflix

---
+++ eflix/eflix/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Akka.Actor;
+
+namespace eflix
+{
+    
+    class Program
+    {
+        private static ActorSystem _eflixActorSystem;
+
+        static void Main(string[] args)
+        {
+            _eflixActorSystem = ActorSystem.Create("eflix");
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
