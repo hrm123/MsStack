87b1afeef104ce0563077364bfb294305dce443c
h_r_m <pitechworks@gmail.com>
2023-08-23 19:40:53 -0700

    puzzl solutions

---
+++ AlgoDemos/AlgoDemos/codecamp/GenerateSubsets.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDemos.codecamp
+{
+    public class GenerateSubsets
+    {
+        /*
+         *  time - 125 ms - beats 90% of users with C#. Memory 43.13 - beats 16% of users with C#
+         */
+        IList<IList<int>> output = new List<IList<int>>();
+        int[] _nums;
+        int _n;
+
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            _nums = nums;
+            _n = _nums.Length;
+            //add empty set
+            List<int> empty = new List<int>();
+            output.Add(empty);
+
+            IList<int> arr = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr.Clear();
+                GenerateSubset(i + 1, 0, -1, arr);
+            }
+            return output;
+        }
+
+        // bs = batch size, cd = current depth, used
+        private void GenerateSubset(int bs, int cd, int prevIndex, IList<int> arr)
+        {
+            if (cd == bs)
+            {
+                // Console.WriteLine(s);
+                IList<int> arrStored = arr.ToList();
+                output.Add(arrStored);
+                return;
+            }
+            for (int i = 0; i < _n; i++)
+            {
+                if (i <= prevIndex) continue;
+                arr.Add(_nums[i]);
+                GenerateSubset(bs, cd + 1, i, arr);
+                arr.RemoveAt(arr.Count - 1);
+            }
+        }
+
+    }
+}
---
+++ AlgoDemos/AlgoDemos/codecamp/LargestRectArea.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDemos.codecamp
+{
+    /*
+     * 
+     *  95 / 98 testcases passed
+     *  time limit exceeded for huge testcase
+    */
+    public class LargestRectArea
+    {
+        int[] _heights;
+        public int LargestRectangleArea(int[] heights)
+        {
+            _heights = heights;
+            RecursiveLargestArea(0, heights.Length - 1);
+            return _largest;
+        }
+
+        int _largest = 0;
+
+        public void RecursiveLargestArea(int start, int end)
+        {
+            // Console.WriteLine($"start={start},end={end}");
+
+            if (start > end)
+            {
+                // fallacy of recursion
+                return;
+            }
+            if (start == end)
+            { // only one bar
+                if (_heights[end] * 1 > _largest)
+                {
+                    _largest = _heights[end];
+                }
+                return;
+            }
+
+
+
+            int minHtIndex = getMinimumHtIndex(_heights, start, end);
+            if (minHtIndex == -1)
+            {
+                return;
+            }
+
+            int minHtArea = GetAreaOfMinHtbarAtIndex(minHtIndex, start, end);
+            // Console.WriteLine($"minHtArea={minHtArea}");
+            if (minHtArea > _largest)
+            {
+                _largest = minHtArea;
+            }
+
+            // split array on both sides of min size indes
+            RecursiveLargestArea(start, minHtIndex - 1);
+            RecursiveLargestArea(minHtIndex + 1, end);
+
+        }
+
+        private int GetAreaOfMinHtbarAtIndex(int index, int start, int end)
+        {
+            return (end - start + 1) * _heights[index];
+        }
+
+        private int getMinimumHtIndex(int[] a, int start, int end)
+        {
+            int minHt = 100000, minHtIndex = -1;
+            for (int j = start; j <= end; j++)
+            {
+                if (a[j] < minHt) { minHt = a[j]; minHtIndex = j; }
+            }
+            return minHtIndex;
+        }
+    }
+}
