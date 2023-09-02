fe4cd107622d4dc764a1a5d778f31e89ac1642a0
h_r_m <pitechworks@gmail.com>
2023-09-02 14:58:06 -0700

    puzzle trimmed down code

---
+++ AlgoDemos/AlgoDemos/codecamp/CanPlaceFlowers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDemos.codecamp
+{
+    internal class CanPlaceFlowersSoln
+    {
+        /*
+         * Runtime - 98ms (beats 82% users with C#). Memory 46 MB (beats 66% users with C#)
+         */
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int ctr = 0;
+            int i = 0;
+            int l = flowerbed.Length;
+            if (n == 0) { return true; }
+            if (l == 0) { return n == 0 ? true : false; }
+            if (l == 1)
+            {
+                if (flowerbed[0] == 0)
+                {
+                    ctr++;
+                    if (ctr >= n)
+                    {
+                        flowerbed[0] = 1;
+                        return true;
+                    }
+                }
+            }
+
+
+            while (i <= l - 1)
+            {
+                if (ctr >= n)
+                {
+                    return true;
+                }
+                if (i == 0 || i == l - 2)
+                {
+                    if (flowerbed[i] == 0 && flowerbed[i + 1] == 0)
+                    {
+                        ctr++;
+                        flowerbed[i == 0 ? 0 : i + 1] = 1;
+                    }
+                }
+                else if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+                {
+                    ctr++;
+                    flowerbed[i] = 1;
+                }
+                i++;
+            }
+            return false;
+        }
+    }
+}
---
+++ AlgoDemos/AlgoDemos/codecamp/PlusOne.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDemos.codecamp
+{
+    /*
+     * 127 ms (Beats 72% users of C#). Memory 42 MB (beats 50% of users of C#)
+     */
+    internal class PlusOneSoln
+    {
+        int[] _d;
+        public int[] PlusOne(int[] digits)
+        {
+            int l = digits.Length;
+            _d = digits;
+            PlusOneRecursive(l - 1);
+            return _d;
+        }
+
+        private void PlusOneRecursive(int r)
+        {
+            if (_d[r] == 9)
+            {
+                if (r == 0)
+                {
+                    // new array need to be created
+                    int[] tmp = new int[_d.Length + 1];
+                    tmp[0] = 1;
+                    _d.CopyTo(tmp, 1);
+
+                    tmp[1] = 0;
+                    _d = tmp;
+                }
+                else
+                {
+                    _d[r] = 0;
+                    PlusOneRecursive(r - 1);
+                }
+            }
+            else
+            {
+                _d[r] = _d[r] + 1;
+            }
+        }
+    }
+}
