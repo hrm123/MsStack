cc25d21ed0f61d0f65fadd8fac3f1699fafbf1a7
h_r_m <pitechworks@gmail.com>
2023-09-03 16:15:17 -0700

    coin change - second attempt - wrong recursive code - since I took greedy approach though lesser

--- AlgoDemos/AlgoDemos/codecamp/CoinChange.cs
+++ AlgoDemos/AlgoDemos/codecamp/CoinChange.cs
@@ -8,56 +8,84 @@ namespace AlgoDemos.codecamp
 {
     public class CoinChangeSoln
     {
+
+        Dictionary<string, int> solutionCache = new Dictionary<string, int>();
+        int[] _coins;
         public int CoinChange(int[] coins, int amount)
         {
             Dictionary<int, int> CoinQty = new Dictionary<int, int>();
 
             int l = coins.Length;
+            Array.Sort(coins);
+            // Array.Reverse(coins);
+            _coins = coins;
 
             for (int i = 0; i < l; i++)
             {
                 CoinQty[coins[i]] = 0;
             }
+            int recursionDepth = 0;
+            int numCoins = RecursiveSoln(l - 1, amount, 0, recursionDepth);
+            return numCoins;
+        }
 
-            Array.Sort(coins);
-            Array.Reverse(coins);
-
-            int total = amount;
-
-            // Console.WriteLine($"l={coins.Length}");
-            for (int i = 0; i < l; i++)
+        private int RecursiveSoln(int end, int total, int numCoins, int recursionDepth)
+        {
+            Console.WriteLine($"end={end} amt={_coins[end]} total={total} numCoins={numCoins} recursionDepth={recursionDepth}");
+            /*
+            if(solutionCache.ContainsKey(end+","+total)){
+                return solutionCache[end+","+total];
+            }
+            */
+            int response = -1;
+            if (end == 0)
             {
-                int denom = coins[i];
-                Console.WriteLine($"denom={denom} total={total}");
-                if (total == 0)
+                //last element of _coins array.. if it reduces total to zero then return numCoins else  return -1
+                if (total % _coins[end] == 0)
                 {
-                    break;
+                    response = numCoins + total / _coins[end];
                 }
-
-                if (total >= denom)
-                {
-                    int tmp = total % denom;
-
-                    CoinQty[denom] = total / denom;
-                    total = tmp;
-                    Console.WriteLine($"denom={denom} total={total} qty={CoinQty[denom]}");
-                }
+                else { response = -1; }
+                solutionCache.Add(end + "," + total, response);
+                return response;
             }
-
-            if (total != 0)
+            if (total < _coins[end])
             {
-                return -1;
+                //no change in numCoins since current coin cannot help in reducing total
+                //Console.WriteLine($"no change in numCoins");
+                int excludesCurrentCoin = RecursiveSoln(end - 1, total, numCoins, recursionDepth + 1);
+
+                response = excludesCurrentCoin;
+                solutionCache.Add(end + "," + total, response);
+                return response;
             }
 
-            int totalCoins = 0;
-            foreach (var (key, value) in CoinQty)
+            int resultIncludesCurrentCoin = RecursiveSoln(end - 1, total % _coins[end], numCoins + total / _coins[end], recursionDepth + 1);
+            int resultExcludesCurrentCoin = RecursiveSoln(end - 1, total, numCoins, recursionDepth + 1);
+            if (resultIncludesCurrentCoin == -1 && resultExcludesCurrentCoin == -1)
             {
-                totalCoins += value;
+                solutionCache.Add(end + "," + total, -1);
+                return -1; // this route not possible to get solution
             }
-            return totalCoins;
-
-
-
+            else if (resultIncludesCurrentCoin == -1)
+            {
+                // other result wins
+                solutionCache.Add(end + "," + total, resultExcludesCurrentCoin);
+                return resultExcludesCurrentCoin;
+            }
+            else if (resultExcludesCurrentCoin == -1)
+            {
+                // other result wins
+                solutionCache.Add(end + "," + total, resultIncludesCurrentCoin);
+                return resultIncludesCurrentCoin;
+            }
+            else
+            {
+                // minimum result wins
+                response = resultIncludesCurrentCoin <= resultExcludesCurrentCoin ? resultIncludesCurrentCoin : resultExcludesCurrentCoin;
+                solutionCache.Add(end + "," + total, response);
+                return response;
+            }
         }
     }
 }
