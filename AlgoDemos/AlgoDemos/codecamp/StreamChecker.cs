fe969272e8b313ac9dab92b455454adc4da98e1e
h_r_m <pitechworks@gmail.com>
2023-09-12 18:30:13 -0700

    endswith algorithm works

--- AlgoDemos/AlgoDemos/codecamp/StreamChecker.cs
+++ AlgoDemos/AlgoDemos/codecamp/StreamChecker.cs
@@ -8,7 +8,9 @@ namespace AlgoDemos.codecamp
 {
     /// <summary>
     /// 1032. Stream of Characters - Class checks if given stream of characters ends with any of words given 
-    /// First attempt - failed on basic tests 
+    /// First attempt - failed on basic tests (typo)
+    /// Second Attempt - timed out after 15/18 testcases(because of typo)
+    /// Third attempt - 2493 ms (beats 8% of C# users). 166 MB - beats 22% of C# users
     /// </summary>
     /// 
     public class StreamChecker
@@ -16,7 +18,7 @@ namespace AlgoDemos.codecamp
 
         Dictionary<string, HashSet<int>> _matcher = new Dictionary<string, HashSet<int>>();
         string[] _words;
-
+        int _minWordLength = 100000, _maxWordLength = 0;
         public string Reverse(string str)
         {
             char[] chars = str.ToCharArray();
@@ -24,16 +26,19 @@ namespace AlgoDemos.codecamp
             return new string(chars);
         }
 
-
         public StreamChecker(string[] words)
         {
             _words = words;
             // create matcher = Hash<char+indexinword,set of ints> which gives index of all words that have particular character at particular position
             // have to reverse words since it is easier to match
             int indexOfword = 0;
+
+
             foreach (string wordReverse in words)
             {
                 string wordCurrent = Reverse(wordReverse);
+                _minWordLength = (wordCurrent.Length < _minWordLength) ? wordCurrent.Length : _minWordLength;
+                _maxWordLength = (wordCurrent.Length > _maxWordLength) ? wordCurrent.Length : _maxWordLength;
                 for (int i = 0; i < wordCurrent.Length; i++)
                 {
                     HashSet<int> tmp = null;
@@ -52,6 +57,7 @@ namespace AlgoDemos.codecamp
                 indexOfword++;
             }
 
+            Console.WriteLine($"hash created {_matcher.Count()} , minWordLength={_minWordLength}, maxWordLength={_maxWordLength}");
             /*
             foreach(var (key,value) in _matcher){
                 Console.WriteLine($"key={key} - value={String.Join<int>(",", value)}");
@@ -69,17 +75,33 @@ namespace AlgoDemos.codecamp
             HashSet<int> prevSet = null;
             int j = 0;
             streamedChars.Add(letter);
-            // Console.WriteLine($"count={streamedChars.Count()}");
+            if (streamedChars.Count() < _minWordLength) return false;
+            HashSet<int> currentSet = null;
+            HashSet<int> tmp = null;
+            //  Console.WriteLine($"count={streamedChars.Count()}");
             for (int i = streamedChars.Count() - 1; i >= 0; i--)
             {
+                /*
+                if(j>_minWordLength){
+                    Console.WriteLine("more than min word length");
+                }
+                */
+                if (j > _maxWordLength)
+                {
+                    Console.WriteLine("more than max word length");
+                }
                 // the first streamed character should match last character of words
                 // second  streamed character should match 2nd lst character of words etc
                 string key = streamedChars[i] + "" + j;
-                HashSet<int> currentSet = null;
+                currentSet = null;
                 if (_matcher.ContainsKey(key))
                 {
                     currentSet = _matcher[key];
                     //Console.WriteLine($" key={key},  current set={ String.Join<int>(",", currentSet)}");   
+                }
+                else
+                {
+                    return false;
                 }
 
 
@@ -91,10 +113,14 @@ namespace AlgoDemos.codecamp
 
 
                 //check if current set has any word  of length i.. if yes that is solution
-                HashSet<int> tmp = new HashSet<int>(currentSet);
+                tmp = new HashSet<int>(currentSet);
                 if (prevSet != null)
                 {
                     tmp.IntersectWith(prevSet);
+                    if (tmp == null || tmp.Count == 0)
+                    {
+                        return false;
+                    }
                     // Console.WriteLine($" intersect set={ String.Join<int>(",", tmp)}"); 
                     //check if current set has any word  of length i.. if yes that is solution
                     if (ExistsWordOfGivenLength(tmp, j + 1))
@@ -132,6 +158,11 @@ namespace AlgoDemos.codecamp
         }
     }
 
+    /**
+     * Your StreamChecker object will be instantiated and called as such:
+     * StreamChecker obj = new StreamChecker(words);
+     * bool param_1 = obj.Query(letter);
+     */
     /**
      * Your StreamChecker object will be instantiated and called as such:
      * StreamChecker obj = new StreamChecker(words);
