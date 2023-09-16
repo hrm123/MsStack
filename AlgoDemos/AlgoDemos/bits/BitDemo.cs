5d25db5d3f739a830b066a5d5d24022af7c546b0
h_r_m <pitechworks@gmail.com>
2023-09-16 08:19:00 -0700

    Minimum Swaps To Make Sequences Increasing - kind of dp

--- AlgoDemos/AlgoDemos/bits/BitDemo.cs
+++ AlgoDemos/AlgoDemos/bits/BitDemo.cs
@@ -6,7 +6,122 @@ using System.Threading.Tasks;
 
 namespace AlgoDemos.bits
 {
-    internal class BitDemo
+    /// <summary>
+    /// C# bit operators are defined for int types (int, uint, long, ulong). They acan also be applied on few other 
+    /// types (sbyte, byte, short, ushort, char) which
+    /// are first converted to Integer and then bit wise operations applied 
+    /// Operators -  [ OR = | ] [ AND = & ] [ XOR = ^ ] [ COMPLEMENT = ~ ] [ LEFTSHIFT = << ] [ RIGHTSHIFT = >> ] [UNSIGNEDRIGHTSHIFT = >>> ]
+    /// Bitwise and shift operators never cause overflow
+    /// Operator precedence (highest to lowest) - '~' , '<<'/'>>'/'>>>' , & , ^ , |
+    /// </summary>
+    public class BitDemo
     {
+
+        /// <summary>
+        /// Toggles all bits of operand - making 1s to be 0's and viceversa
+        /// </summary>
+        public static void Complement_Demo()
+        {
+            uint a = 0b_0000_1111_0000_1111_0000_1111_0000_1100; // uint is 32 bit
+            uint b = ~a;
+            Console.WriteLine(Convert.ToString(b, toBase: 2));
+            // Output: 
+            // 11110000111100001111000011110011
+        }
+
+        /// <summary>
+        /// Shifts bits of left-hand operand by number of bits defined by right-hand operand. This operation
+        /// discards the higher order bits that are outside the range of result type and sets lower-order 
+        /// empty bit positions  to 0
+        /// </summary>
+        public static void LeftShift_Demo()
+        {
+            int a = 1;
+            Console.WriteLine($"Before: {Convert.ToString(a, toBase: 2)}");
+            int b = a << 30;
+            Console.WriteLine($"After left shift by 30 places: {Convert.ToString(b, toBase: 2)}");
+
+
+            uint x = 0b_1100_1001_0000_0000_0000_0000_0001_0001;
+            Console.WriteLine($"Before: {Convert.ToString(x, toBase: 2)}");
+
+            uint y = x << 4;
+            Console.WriteLine($"After:  {Convert.ToString(y, toBase: 2)}");
+            // Output:
+            // Before: 11001001000000000000000000010001
+            // After:  10010000000000000000000100010000
+        }
+
+        /// <summary>
+        /// '>>' operator shifts its left hand operand right by the number of bits defined by its right hand
+        /// operand.  It discards lower order bits that go out of  last position after shift. How the higher order bits 
+        /// are handled depends on datatype. 
+        /// For int/long - >> performs arithmetic shift - if number is -ve then  higher order empty bits become 1. If number 
+        /// is positive then higher order empty bits become 0.
+        /// For uint/ulong >> performs logical shift - higher order empty positions are always set to 0
+        /// </summary>
+        public static void RightShift_Demo()
+        {
+            uint x = 0b_1001;
+            Console.WriteLine($"Before: {Convert.ToString(x, toBase: 2),4}");
+
+            uint y = x >> 2;
+            Console.WriteLine($"After:  {Convert.ToString(y, toBase: 2).PadLeft(4, '0'),4}");
+            // Output:
+            // Before: 1001
+            // After:  0010
+        }
+
+
+        /// <summary>
+        /// '>>>' always performs logical shift. That means higher order empty bits are set to 0. Lower order
+        /// bits that go out of last position are discarded. >>> performs arithmetic shift if lef thand operand is signed type
+        /// Only available in C# 11.0
+        /// </summary>
+        public static void UnsignedRightShift_Demo()
+        {
+            int x = -8;
+            Console.WriteLine($"Before:    {x,11}, hex: {x,8:x}, binary: {Convert.ToString(x, toBase: 2),32}");
+
+            int y = x >> 2;
+            Console.WriteLine($"After  >>: {y,11}, hex: {y,8:x}, binary: {Convert.ToString(y, toBase: 2),32}");
+
+            int z = 0; // x >>> 2;
+            Console.WriteLine($"After >>>: {z,11}, hex: {z,8:x}, binary: {Convert.ToString(z, toBase: 2).PadLeft(32, '0'),32}");
+            // Output:
+            // Before:             -8, hex: fffffff8, binary: 11111111111111111111111111111000
+            // After  >>:          -2, hex: fffffffe, binary: 11111111111111111111111111111110
+            // After >>>:  1073741822, hex: 3ffffffe, binary: 00111111111111111111111111111110
+        }
+
+        public void And_Demo()
+        {
+            uint a = 0b_1111_1000;
+            uint b = 0b_1001_1101;
+            uint c = a & b;
+            Console.WriteLine(Convert.ToString(c, toBase: 2));
+            // Output:
+            // 10011000
+        }
+
+        public void Xor_Demo()
+        {
+            uint a = 0b_1111_1000;
+            uint b = 0b_0001_1100;
+            uint c = a ^ b;
+            Console.WriteLine(Convert.ToString(c, toBase: 2));
+            // Output:
+            // 11100100
+        }
+        public void Or_Demo()
+        {
+            uint a = 0b_1010_0000;
+            uint b = 0b_1001_0001;
+            uint c = a | b;
+            Console.WriteLine(Convert.ToString(c, toBase: 2));
+            // Output:
+            // 10110001
+        }
+
     }
 }
--- AlgoDemos/AlgoDemos/codecamp/MinSwapsAscArray.cs
+++ AlgoDemos/AlgoDemos/codecamp/MinSwapsAscArray.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,86 +9,107 @@ namespace AlgoDemos.codecamp
 {
     //801. Minimum Swaps To Make Sequences Increasing
 
-    public class MinSwapsAscArray
+    public class MinSwapsAcsArray
     {
         int[] _a1;
         int[] _a2;
+        int[] dp;
 
         public int MinSwap(int[] nums1, int[] nums2)
         {
             _a1 = nums1;
             _a2 = nums2;
-            dp(0, 0);
-            return _minSwaps;
+            dp = new int[_a1.Length];
+            dp[0] = 0; // no swaps when only one element arrays
+            return soln();
         }
 
-        void swap(int depth)
+
+        bool swap(int depth)
         {
             // Console.WriteLine($"swap depth={depth}");
+            if (depth < 0 || depth >= _a1.Length)
+            {
+                return false; // nothing to do
+            }
             int tmp = _a1[depth];
             _a1[depth] = _a2[depth];
             _a2[depth] = tmp;
+            return true;
         }
 
         int _minSwaps = Int32.MaxValue;
-
+        bool endRecursion = false;
         bool InOrder(int depth)
         {
 
-            if (depth > _a1.Length || depth < 0)
+            if (depth >= _a1.Length || depth < 0)
             {
-                // Console.WriteLine($"error at {depth}");
+                Console.WriteLine($"error at {depth}");
                 return false;
             }
 
-            if (depth < 2)
-            {
-                return true;
-            }
             // from start of array to  depth  each array should have ascending order
-            for (int i = 0; i <= depth - 2; i++)
+            for (int i = 0; i <= depth - 1; i++)
             {
-                if (_a1[i] >= _a1[i + 1] || _a2[i] >= _a2[i + 1])
+                if (_a1[i] >= _a1[i + 1])
                 {
+                    Console.WriteLine($"{_a1[i]},{_a1[i + 1]} mismatch");
+                    return false;
+                }
+                if (_a2[i] >= _a2[i + 1])
+                {
+                    Console.WriteLine($"{_a2[i]},{_a2[i + 1]} mismatch");
                     return false;
                 }
             }
             return true;
         }
 
-        Dictionary<string, bool> _cache = new Dictionary<string, bool>();
 
-        void dp(int depth, int total)
+        int soln()
         {
-            if (depth > _a1.Length || _minSwaps == 0)
+            for (int x = 1; x < _a1.Length; x++)
             {
-                return; // end of recursion 
-            }
+                bool inOrder = InOrder(x);
 
-            // Console.WriteLine($"depth={depth}");
-
-            bool inOrder = InOrder(depth);
-            if (!inOrder)
-            {
-                return; // end of recursion - failure
-            }
-
-            if (inOrder && (depth == _a1.Length))
-            {
-                if (_minSwaps > total)
+                if (inOrder)
                 {
-                    _minSwaps = total;
+                    Console.WriteLine($"in order at depth={x}");
+                    dp[x] = dp[x - 1];
+                    continue;
                 }
-                // Console.WriteLine($"Reached order at {depth} with value {_minSwaps}");
-                return;  // end of recursion - success
 
+                Console.WriteLine($"not in order at depth={x}");
+                //choices - swap (i-1) / (i)  to see which on yields best result
+                int numswaps = 0;
+                numswaps += swap(x) ? 1 : 0; // with only (i) swapped
+                Console.WriteLine($"swapped {_a1[x]},{_a2[x]}");
+                if (!InOrder(x))
+                {
+                    //unswap (i)
+                    numswaps -= swap(x) ? 1 : 0;
+                    Console.WriteLine($"unswapped {_a1[x]},{_a2[x]}");
+
+                    numswaps += swap(x - 1) ? 1 : 0; // now we have swapped  i-1
+                    Console.WriteLine($"swapped {_a1[x - 1]},{_a2[x - 1]}");
+
+                    if (!InOrder(x))
+                    {
+                        //swap (i)
+                        numswaps -= swap(x) ? 1 : 0;
+                        Console.WriteLine($"swapped both {_a1[x - 1]},{_a2[x - 1]} & {_a1[x]},{_a2[x]}");
+                        if (!InOrder(x))
+                        { // // now we have swapped both i / (i-1)
+                          //fail
+                            return -1;
+                        }
+                    }
+                }
+                Console.WriteLine($"numswaps={numswaps} at depth={x}");
+                dp[x] = dp[x - 1] + numswaps;
             }
-
-            dp(depth + 1, total); // without swap
-            swap(depth);
-            dp(depth + 1, total + 1);
-
+            return dp[_a1.Length - 1];
         }
-
     }
 }
