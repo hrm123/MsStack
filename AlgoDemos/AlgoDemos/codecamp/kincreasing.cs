752d5995f3938a463bc1f5333f4395e564cb6486
h_r_m <pitechworks@gmail.com>
2023-09-19 16:24:50 -0700

    k-array increasing

--- AlgoDemos/AlgoDemos/codecamp/kincreasing.cs
+++ AlgoDemos/AlgoDemos/codecamp/kincreasing.cs
@@ -39,90 +39,42 @@ namespace AlgoDemos.codecamp
             int operationsReqd = 0;
             foreach (List<int> current in subarrays)
             {
-                operationsReqd += soln(current.ToArray());
+                operationsReqd += LengthOfLIS(current.ToArray());
             }
             return operationsReqd;
         }
-
-        int[,] _dp; // 0 => i-1 is changed (to min number of processed array)/ 1 => i is changed (to same as i-1) / 2 => both changed (both made '1')
-
-
-        bool IsInOrder(int[] arr, int depth, int mode)
+        public int LengthOfLIS(int[] nums)
         {
-            if (mode == 0) // mode used on (depth-1)
+
+            int n = nums.Length;
+            List<int> list = new List<int>();
+            list.Add(nums[0]);
+            for (int i = 1; i < n; i++)
             {
-                return (arr[depth] >= arr[depth - 1]);
+                if (nums[i] > list[list.Count() - 1])
+                {
+                    list.Add(nums[i]);
+                }
+                else
+                {
+                    int low = 0;
+                    int high = list.Count() - 1;
+                    while (low <= high)
+                    {
+                        int mid = (high + low) / 2;
+                        if (list[mid] < nums[i])
+                        {
+                            low = mid + 1;
+                        }
+                        else
+                        {
+                            high = mid - 1;
+                        }
+                    }
+                    list[low] = nums[i];
+                }
             }
-            if (mode == 1) // both numbers i,i-1 have been made equal
-            {
-                return true;
-            }
-            if (mode == 2) // depth-1 would have been 1 (since both are changed both will be made equal to 1)
-            {
-                return true;
-            }
-            return false;
-        }
-        int _currentMax = 1;
-        const int CHANGETOMAX = 0, CHANGETOONE = 1, NOCHANGE = 2;
-
-
-        int GetMin(int depth)
-        {
-            return Math.Min(_dp[depth, CHANGETOMAX], Math.Min(_dp[depth, CHANGETOONE], _dp[depth, NOCHANGE]));
-        }
-
-        Dictionary<int, int> _counter = new Dictionary<int, int>();
-        Tuple<int, int> GetMinMax(int[] arr)
-        {
-            int min = 100000;
-            int max = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                min = arr[i] < min ? arr[i] : min;
-                max = arr[i] > max ? arr[i] : max;
-                _counter[arr[i]] = 1 + _counter[arr[i]];
-            }
-            return new Tuple<int, int>(min, max);
-        }
-
-        int soln(int[] arr)
-        {
-            for (int i = 1; i <= arr.Length; i++)
-            {
-                _counter[i] = 0;
-            }
-            var minmax = GetMinMax(arr);
-            _currentMax = minmax.Item1;
-            _dp = new int[arr.Length, 3];
-
-
-            _dp[0, CHANGETOMAX] = (arr[0] != _currentMax) ? 1 : 0; ;
-            _dp[0, CHANGETOONE] = (arr[0] != 1) ? 1 : 0;
-            _dp[0, NOCHANGE] = 0;
-
-            for (int i = 1; i < arr.Length; i++)
-            {
-
-                //3 options are there for current index
-
-                //1. change previous/current number to be same (applies to any previous state)
-                _currentMax = (i>1) ? ( (arr[i] > _currentMax) ? _currentMax : arr[i-1]) : arr[i];
-                _dp[i, CHANGETOMAX] = Math.Min(_dp[i - 1, CHANGETOMAX], _dp[i - 1, CHANGETOONE]) + (arr[i] == _currentMax  ? 0 : 1);
-                _dp[i, CHANGETOMAX] = Math.Min(_dp[i, CHANGETOMAX],
-                _dp[i - 1, NOCHANGE] + ((arr[i] == _currentMax) ? 0 : 1) + ((arr[i - 1] == _currentMax) ? 0 : 1));
-
-                //2. change both numbers to (1,1) .. possible based on previous (1,1)
-                _dp[i, CHANGETOONE] = _dp[i - 1, CHANGETOONE] + (arr[i] == 10 ? 0 : 1);
-
-                //3. Dont change current number  (could be possible / not possible) - previous (change to one case / previous no change case / previous changes to current greatest case)
-
-                _dp[i, NOCHANGE] = (arr[i] < arr[i - 1]) ? 100000 : _dp[i - 1, NOCHANGE]; // previous no change case
-                _dp[i, NOCHANGE] = Math.Min(_dp[i, NOCHANGE], _dp[i - 1, CHANGETOONE]); // previous change to one case
-                _dp[i, NOCHANGE] = Math.Min(_dp[i, NOCHANGE], (arr[i] < _currentMax) ? 100000 : _dp[i - 1, CHANGETOMAX]); // previouschange to max case
-
-            }
-            return GetMin(arr.Length - 1);
+            return list.Count();
         }
 
 
