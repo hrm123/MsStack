783b6d6637139a960df397408d597e2155a867c3
h_r_m <pitechworks@gmail.com>
2023-09-28 08:41:35 -0700

    NonOverlappingIntvls logical soln - end time based

--- AlgoDemos/AlgoDemos/codecamp/NonOverlappingIntvls.cs
+++ AlgoDemos/AlgoDemos/codecamp/NonOverlappingIntvls.cs
@@ -10,7 +10,8 @@ namespace AlgoDemos.codecamp
     {
         public static void TestCase()
         {
-            int[][] intvls = new int[][] { new int[] { -52, 31 }, new int[] { -73, -26 }, new int[] { 82, 97 }, new int[] { -65, -11 }, new int[] { -62, -49 }, new int[] { 95, 99 }, new int[] { 58, 95 }, new int[] { -31, 49 }, new int[] { 66, 98 }, new int[] { -63, 2 }, new int[] { 30, 47 }, new int[] { -40, -26 } };
+            int[][] intvls = new int[][] { new int[] { -52, 31 }, new int[] { -73, -26 }, new int[] { 82, 97 }, new int[] { -65, -11 }, new int[] { -62, -49 },
+                new int[] { 95, 99 }, new int[] { 58, 95 }, new int[] { -31, 49 }, new int[] { 66, 98 }, new int[] { -63, 2 }, new int[] { 30, 47 }, new int[] { -40, -26 } };
             NonOverlappingIntvls noi = new NonOverlappingIntvls();
             int answer = noi.EraseOverlapIntervals(intvls);
           }
@@ -30,75 +31,25 @@ namespace AlgoDemos.codecamp
                 points.Add(tup);
             }
 
-            //sort points
-            points = points.OrderBy(p => p.Item1).ThenBy(p => p.Item2).ToList();
+            //sort points based on end time
+            points = points.OrderBy(p => p.Item2).ToList(); 
 
-            // remove multiple routes starting from same point (retain only the first of such)
-            i = 0;
-            List<Tuple<int, int>> pointsNew = new List<Tuple<int, int>>();
-            int removed = 0;
-            Dictionary<int, int> counts = new Dictionary<int, int>();
-            while (i < points.Count())
-            {
-                pointsNew.Add(points[i]);
-                i++;
-                while (i < points.Count() && points[i].Item1 == points[i - 1].Item1)
-                {
-                    removed++;
-                    i++;
-                }
-            }
-
-            //create hash counter based on numebr of starts and number of ends at particular point
-            for (i = 0; i < pointsNew.Count(); i++)
-            {
-                if (!counts.ContainsKey(pointsNew[i].Item1)) { counts[pointsNew[i].Item1] = 0; }
-                if (!counts.ContainsKey(pointsNew[i].Item2)) { counts[pointsNew[i].Item2] = 0; }
-                counts[pointsNew[i].Item1] += 1;
-                counts[pointsNew[i].Item2] -= 1;
-            }
 
             //remove overlaps based on depth at any given point
-            int depth = 0;
-            List<Tuple<int, int>> removeList = new List<Tuple<int, int>>();
-            for (i = 0; i < pointsNew.Count(); i++)
+            int endtime = Int32.MinValue, result = 0;
+            for (i = 0; i < points.Count(); i++)
             {
 
-                if(i>0 && pointsNew[i].Item1 > pointsNew[i - 1].Item2)
+                if (points[i].Item1 >= endtime)
                 {
-                    depth += counts[pointsNew[i-1].Item2];
+                    endtime = points[i].Item2; 
                 }
-
-                depth += counts[pointsNew[i].Item1];
-                if (depth > 1)
+                else
                 {
-                    //add point that has max Item2 to remove list
-                    if (pointsNew[i].Item2 > pointsNew[i-1].Item2 )
-                    {
-                        //remove pointsNew[i]
-                        if (!removeList.Contains(pointsNew[i])) // some bug here - why is removed edge again there in flow
-                        {
-                            removeList.Add(pointsNew[i]);
-                        }
-                        counts[pointsNew[i].Item2] += 1; // since earlier it ends there -1 was added. Now it does not end there anymore
-                        counts[pointsNew[i].Item1] -= 1;
-
-                    }
-                    else
-                    {
-                        //remove pointsNew[i-1]
-                        if (!removeList.Contains(pointsNew[i-1]))
-                        {
-                            removeList.Add(pointsNew[i - 1]);
-                        }
-                        counts[pointsNew[i - 1].Item2] += 1; // since earlier it ends there -1 was added. Now it does not end there anymore
-                        counts[pointsNew[i - 1].Item1] -= 1;
-
-                    }
-                    depth -= 1;
+                    result++;
                 }
             }
-            return removed + removeList.Count();
+            return result;
         }
     }
 }
