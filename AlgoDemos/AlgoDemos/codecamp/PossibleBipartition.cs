588b4c5bda13512780f61a26fd28406bd61417f1
h_r_m <pitechworks@gmail.com>
2023-10-02 14:39:53 -0700

    dislikes another approach passes 46/74 cases

--- AlgoDemos/AlgoDemos/codecamp/PossibleBipartition.cs
+++ AlgoDemos/AlgoDemos/codecamp/PossibleBipartition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlgoDemos.ExpressionTree;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,117 +12,75 @@ namespace AlgoDemos.codecamp
     /// </summary>
     public class PossibleBipartitionSln
     {
-        public static void TestCase()
+        public static void TestCase1()
         {
-            int[][] arr = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 }, new int[] { 5, 6 }, new int[] { 6, 7 }, new int[] { 8, 9 }, new int[] { 7, 8 } }; // new int[] { 1, 2 }, new int[] { 3, 4 }, new int[] { 5, 6 }, new int[] { 6, 7 }, new int[] { 8, 9 }, new int[] { 7, 8 } };
+            int[][] arr = new int[][] { new int[] { 4, 7 }, new int[] { 4, 8 }, new int[] { 5, 6 }, new int[] { 1, 6 }, new int[] { 3, 7 }, new int[] { 2, 5 }, new int[] { 5, 8 }, new int[] { 1, 2 }, new int[] { 4, 9 }, new int[] { 6, 10 }, new int[] { 8, 10 }, new int[] { 3, 6 }, new int[] { 2, 10 }, new int[] { 9, 10 }, new int[] { 3, 9 }, new int[] { 2, 3 }, new int[] { 1, 9 }, new int[] { 4, 6 }, new int[] { 5, 7 }, new int[] { 3, 8 }, new int[] { 1, 8 }, new int[] { 1, 7 }, new int[] { 2, 4 } };
             int n = 10;
 
             var pbs = new PossibleBipartitionSln();
             Console.WriteLine($"Answer = {pbs.PossibleBipartition(n, arr)}");
         }
 
-
-        public bool PossibleBipartition(int n, int[][] dislikes)
+        public static void TestCase()
         {
-            HashSet<int>[] sets = new HashSet<int>[2] { new HashSet<int>(), new HashSet<int>() };
-            int l1 = -1, l2 = -1;
-            int l1Exists = -1, l2Exists = -1;  // -1 means does not exist. 0 means first set. 1 means second set
-            for (int i = 0; i < dislikes.GetLength(0); i++)
+            int[][] arr = new int[][] { new int[] { 1,2 }, new int[] { 1,3 }, new int[] { 2,3 }};
+            int n = 3;
+
+            var pbs = new PossibleBipartitionSln();
+            Console.WriteLine($"Answer = {pbs.PossibleBipartition(n, arr)}");
+        }
+
+
+
+        public bool PossibleBipartition(int N, int[][] dislikes)
+        {
+            var graph = new List<int>[N + 1];
+            for (var i = 1; i <= N; i++)
             {
-                l1 = dislikes[i][0];
-                l2 = dislikes[i][1];
-                l1Exists = -1;
-                l2Exists = -1;
-
-                if (sets[0].Contains(l1)) { l1Exists = 0; }
-                if (sets[1].Contains(l1)) { l1Exists = 1; }
-
-                if (sets[0].Contains(l2)) { l2Exists = 0; }
-                if (sets[1].Contains(l2)) { l2Exists = 1; }
-
-                // Console.WriteLine($"l1={l1},l2={l2}, l1Exists={l1Exists}, l2Exists={l2Exists}");
-                Console.WriteLine($"s1 = {string.Join("", sets[0])} / s2 = {string.Join("", sets[1])}");
-                // Console.WriteLine($"here0");
-
-                if (l1Exists != -1 && l2Exists != -1)
-                { //both already there in separate sets
-                    if (l1Exists == l2Exists)
-                    {
-                        // try to see if swapping l1 to another set (and all l1 dislikes to opposite)
-                        for(int y = 0;y < i; y++)
-                        {
-                            if (dislikes[y][0] == l1)
-                            {
-                                if (!sets[l1Exists].Add(dislikes[y][1]))
-                                {
-                                    return false;
-                                }
-                                sets[l1Exists == 0 ? 1 : 0].Remove(dislikes[y][1]);
-
-                            }
-                            if (dislikes[y][1] == l1)
-                            {
-                                if (!sets[l1Exists].Add(dislikes[y][0]))
-                                {
-                                    return false;
-                                }
-                                sets[l1Exists == 0 ? 1 : 0].Remove(dislikes[y][0]);
-                            }
-
-                        }
-                        sets[l1Exists == 0 ? 1 : 0].Add(l1);
-                        sets[l1Exists ].Remove(l1);
-
-                        //now check if all dislikes are still honored
-                        for (int y = 0; y < i; y++)
-                        {
-                            int l1_1 = dislikes[y][0];
-                            int l2_1 = dislikes[y][1];
-                            if ((sets[0].Contains(l1_1) && sets[0].Contains(l2_1)) ||
-                                (sets[1].Contains(l1_1) && sets[1].Contains(l2_1)))
-                            {
-                                return false;
-                            }
-                        }
-                        //if we are here that means conflict got resolved
-                     }
-                    continue;
-                }
-
-                // Console.WriteLine($"here1");
-                if (l1Exists != -1 && l2Exists == -1)
-                { // l1 exists
-                  // we can put l2 in other set only
-
-                    if (!sets[l1Exists == 0 ? 1 : 0].Add(l2))
-                    {
-                        return false;
-                    }
-                    continue;
-                }
-
-                // Console.WriteLine("here2");
-                if (l1Exists == -1 && l2Exists != -1)
-                { // l2 alraedy exists
-                  // l1 can only be put in other set
-                    if (!sets[l2Exists == 0 ? 1 : 0].Add(l1))
-                    {
-                        return false;
-                    }
-                    continue;
-                }
-                // Console.WriteLine($"here3");
-                if (l1Exists == -1 && l2Exists == -1)
-                { // both dont exist.. so we can put either in either set
-                    Console.WriteLine("both dont exist");
-                    sets[0].Add(l1);
-                    sets[1].Add(l2);
-                    continue;
-                }
-
-                return false;
-
+                graph[i] = new List<int>();
             }
+            foreach (var dislike in dislikes)
+            {
+                graph[dislike[0]].Add(dislike[1]);
+            }
+            Dictionary<int, bool> visited = new Dictionary<int, bool>();
+            Dictionary<int, bool> recursedNodes = new Dictionary<int, bool>();
+            for (var i = 1; i <= N; i++)
+            {
+                visited.Clear();
+                recursedNodes.Clear();
+                if (!DFS(graph, i, visited, recursedNodes))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool DFS(List<int>[] graph, int node, Dictionary<int, bool> visited, Dictionary<int, bool> recursedNodes  )
+        {
+            if (recursedNodes.ContainsKey(node))
+            {
+                return true; // cycle is there
+            }
+            if (visited.ContainsKey(node))
+            {
+                return false; //no need to further explore from here
+            }
+
+            recursedNodes[node] = true;
+            visited[node] = true;
+
+            foreach (var neighbor in graph[node])
+            {
+                if (!DFS(graph, neighbor, visited, recursedNodes))
+                {
+                    return false;
+                }
+            }
+
+            recursedNodes.Remove(node);
+
             return true;
         }
     }
